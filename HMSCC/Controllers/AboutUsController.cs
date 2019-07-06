97020898749fd382138f54164f1b8952bfecd65a
Zdravko <atanasov.zdr@gmail.com>
2019-07-06 13:35:15 +0300

    Implement AboutUs controller + views

---
+++ HMSCC/Controllers/AboutUsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HMSCC.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Structure()
+        {
+            return base.View();
+        }
+
+        public IActionResult Activities()
+        {
+            return base.View();
+        }
+
+        public IActionResult Documents()
+        {
+            return base.View();
+        }
+
+        public IActionResult DailyCenter()
+        {
+            return base.View();
+        }
+
+        public IActionResult DeclarationAbsencePresence()
+        {
+            return base.View();
+        }
+
+        public IActionResult ProgramsAndProjects()
+        {
+            return base.View();
+        }
+
+        public IActionResult CorruptionSignals()
+        {
+            return base.View();
+        }
+
+        public IActionResult AdministrativeService()
+        {
+            return base.View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return base.View();
+        }
+    }
+}
