b7525dbe45b7853be3a4cebfdcbdd75d70e8562c
Zdravko <atanasov.zdr@gmail.com>
2019-07-06 14:39:17 +0300

    Implement BuyerProfiles controller + views

---
+++ HMSCC/Controllers/BuyerProfilesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HMSCC.Controllers
+{
+    public class BuyerProfilesController : Controller
+    {
+        public IActionResult AnnouncedOrders()
+        {
+            return base.View();
+        }
+
+        public IActionResult AssignedOrders()
+        {
+            return base.View();
+        }
+
+        public IActionResult FinishedOrders()
+        {
+            return base.View();
+        }
+        public IActionResult InternalRegulations()
+        {
+            return base.View();
+        }
+    }
+}
