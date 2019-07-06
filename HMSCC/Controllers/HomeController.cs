096aaaa027577db45de27d0942d5e4a4f56cb6fa
Zdravko <atanasov.zdr@gmail.com>
2019-07-06 12:26:37 +0300

    Implement Index page

--- HMSCC/Controllers/HomeController.cs
+++ HMSCC/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using HMSCC.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HMSCC.Controllers
 {
@@ -12,18 +6,7 @@ namespace HMSCC.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return base.View();
         }
     }
 }
