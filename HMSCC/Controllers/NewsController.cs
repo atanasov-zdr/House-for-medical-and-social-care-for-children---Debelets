28dad51403397ae602b204454fed27d239835bff
Zdravko <atanasov.zdr@gmail.com>
2019-07-06 16:56:22 +0300

    Implement News controller + view

---
+++ HMSCC/Controllers/NewsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HMSCC.Controllers
+{
+    public class NewsController : Controller
+    {
+        [Route("News")]
+        public IActionResult News()
+        {
+            return base.View();
+        }
+    }
+}
