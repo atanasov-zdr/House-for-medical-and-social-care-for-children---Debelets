617d1e569af8b3eff984aa118f53b67e79a1c796
Zdravko <atanasov.zdr@gmail.com>
2019-07-06 14:05:50 +0300

    Implement Galleries controller + views

---
+++ HMSCC/Controllers/GalleriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HMSCC.Controllers
+{
+    public class GalleriesController : Controller
+    {
+        public IActionResult PhotosByProject()
+        {
+            return base.View();
+        }
+
+        public IActionResult PhotosOfDailyCenter()
+        {
+            return base.View();
+        }
+
+        public IActionResult PhotosOfHmscc()
+        {
+            return base.View();
+        }
+    }
+}
