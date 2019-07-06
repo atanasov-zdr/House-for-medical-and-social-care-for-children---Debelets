022271db41ba5ae04697f3cc841b425d49bdef37
Zdravko <atanasov.zdr@gmail.com>
2019-07-06 17:00:48 +0300

    Implement Archives controller + view

---
+++ HMSCC/Controllers/ArchivesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HMSCC.Controllers
+{
+    public class ArchivesController : Controller
+    {
+        [Route("Archive")]
+        public IActionResult Archive()
+        {
+            return base.View();
+        }
+    }
+}
